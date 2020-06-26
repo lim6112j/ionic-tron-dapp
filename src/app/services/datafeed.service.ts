cbf2172bb71abee90fc545e2b9e82b57ce4e7862
byeongcheollim <lim6112j@gmail.com>
2020-06-26 22:02:17 +0900

    angular cli proxy set

--- src/app/services/datafeed.service.ts
+++ src/app/services/datafeed.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, interval } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { map, flatMap, distinctUntilChanged } from 'rxjs/operators';
+interface DataFormat {
+  hash: string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class DatafeedService {
-  apiurl = 'https://api.blockcypher.com/v1/eth/main';
+  // apiurl = 'https://api.blockcypher.com/v1/eth/main';
+  // apiurl = '/api';
+  apiurl = '/v1/eth/main';
+  // obs$ = new Observable();
   constructor(
     private http: HttpClient
   ) { }
   getData() {
-    return this.http.get(this.apiurl);
+    // return this.http.get(this.apiurl);
+    return interval(5000).pipe(
+      flatMap(v => this.http.get(this.apiurl)),
+      distinctUntilChanged((prev: DataFormat, curr: DataFormat) => prev.hash === curr.hash)
+    );
   }
 }
