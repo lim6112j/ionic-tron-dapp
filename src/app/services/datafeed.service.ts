a3d3ae8ada61df997af8ed9b54d1dd1598e06059
byeongcheollim <lim6112j@gmail.com>
2020-06-27 10:49:50 +0900

    web3 added

--- src/app/services/datafeed.service.ts
+++ src/app/services/datafeed.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, interval } from 'rxjs';
+import { Observable, interval, from } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { map, flatMap, distinctUntilChanged } from 'rxjs/operators';
+import { map, flatMap, distinctUntilChanged, tap } from 'rxjs/operators';
+import Web3 from 'web3';
 interface DataFormat {
   hash: string;
 }
+const OPTIONS = {
+  defaultBlock: 'latest',
+  transactionConfirmationBlocks: 1,
+  transactionBlockTimeout: 5,
+  transactionPollingTimeout: 480
+};
+// const web3 = new Web3(new Web3.providers.HttpProvider('http://15.164.229.155:8545'), null, OPTIONS);
+// const web3 = new Web3(new Web3.providers.HttpProvider('https://blockchain.defora.io'), null, OPTIONS);
+const web3 = new Web3(new Web3.providers.HttpProvider('http://3.34.124.190:8545'), null); // Clesson
 @Injectable({
   providedIn: 'root'
 })
@@ -19,9 +29,19 @@ export class DatafeedService {
   ) { }
   getData() {
     // return this.http.get(this.apiurl);
-    return interval(5000).pipe(
-      flatMap(v => this.http.get(this.apiurl)),
-      distinctUntilChanged((prev: DataFormat, curr: DataFormat) => prev.hash === curr.hash)
+    web3.eth.getBlockNumber().then(v => console.log(v));
+    return interval(3000).pipe(
+      flatMap(i => web3.eth.getBlockNumber()),
+      flatMap(v => web3.eth.getBlock(v)),
+      distinctUntilChanged((prev, curr) => prev.hash === curr.hash),
+      // tap(console.log),
     );
+    from(web3.eth.getBlockNumber().then(v => v)).pipe(
+
+    )
+    // return interval(15000).pipe(
+    //   flatMap(v => this.http.get(this.apiurl)),
+    //   distinctUntilChanged((prev: DataFormat, curr: DataFormat) => prev.hash === curr.hash)
+    // );
   }
 }
