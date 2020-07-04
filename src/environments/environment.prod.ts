ecc2a53cdc8a97d845ff67c8b61ddd7d116b3973
byeongcheollim <lim6112j@gmail.com>
2020-07-04 12:14:46 +0900

    environment fireabase added in  prod mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDlUIdUP_MoTW1Swh02tGD4L0OtnWCXQqY",
+    authDomain: "license-delivery.firebaseapp.com",
+    databaseURL: "https://license-delivery.firebaseio.com",
+    projectId: "license-delivery",
+    storageBucket: "license-delivery.appspot.com",
+    messagingSenderId: "409148924422"
+  },
 };
