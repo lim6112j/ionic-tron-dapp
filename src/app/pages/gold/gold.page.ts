59d79f64c4fd8ff446d3404476292d02aee0abef
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2020-07-20 16:21:26 +0900

    complex chart made

--- src/app/pages/gold/gold.page.ts
+++ src/app/pages/gold/gold.page.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import { Platform } from '@ionic/angular';
-
-import * as d3 from 'd3-selection';
-import * as d3Scale from 'd3-scale';
-import * as d3Array from 'd3-array';
-import * as d3Axis from 'd3-axis';
+import * as d3 from 'd3';
 import * as fc from 'd3fc';
+import { ImplicitReceiver } from '@angular/compiler';
+
+// const loadDataEndOfDay = d3.csv(url, d => ({
+//   date: new Date(d.Timestamp * 1000),
+//   volume: Number(d.volume),
+//   high: Number(d.high),
+//   low: Number(d.low),
+//   open: Number(d.open),
+//   close: Number(d.close)
+// }));
 @Component({
   selector: 'app-gold',
   templateUrl: './gold.page.html',
   styleUrls: ['./gold.page.scss'],
 })
-export class GoldPage {
+export class GoldPage implements OnInit{
   headerImage = '../../../assets/gold.png';
   barData = [
     { season: 'S1', viewers: 2500000 },
@@ -33,65 +40,144 @@ export class GoldPage {
   svg: any;
   g: any;
   constructor(
-    private _platform: Platform
+    private _platform: Platform,
+    private _http: HttpClient
   ) {
     this.width = 900 - this.margin.left - this.margin.right;
     this.height = 500 - this.margin.top - this.margin.bottom;
   }
+  ngOnInit(): void {
+    this._http.get('assets/yahoo.csv', { responseType: 'text' }).subscribe(v => {
+      var objs = d3.csvParse(v);
+      console.log(objs);
+      const data = objs.map(d => ({
+        date: new Date(d.Timestamp * 1000),
+        volume: Number(d.volume),
+        high: Number(d.high),
+        low: Number(d.low),
+        open: Number(d.open),
+        close: Number(d.close)
+      }));
+      console.log(data);
+
+      this.createChart(data);
+    });
+  }
 
   ionViewDidEnter() {
-    this.init();
-    this.initAxes();
-    this.drawAxes();
-    this.drawChart();
-  }
-  init() {
-    this.svg = d3.select('#barChart')
-      .append('svg')
-      .attr('width', '100%')
-      .attr('height', '100%')
-      .attr('viewBox', '0 0 900 500');
-    this.g = this.svg.append('g')
-      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
-  }
 
-  initAxes() {
-    this.x = d3Scale.scaleBand().rangeRound([0, this.width]).padding(0.1);
-    this.y = d3Scale.scaleLinear().rangeRound([this.height, 0]);
-    this.x.domain(this.barData.map((d) => d.season));
-    this.y.domain([0, d3Array.max(this.barData, (d) => d.viewers)]);
   }
+  createChart(data) {
 
-  drawAxes() {
-    this.g.append('g')
-      .attr('class', 'axis axis--x')
-      .attr('transform', 'translate(0,' + this.height + ')')
-      .call(d3Axis.axisBottom(this.x))
-      .attr('font-size', '30');
-    this.g.append('g')
-      .attr('class', 'axis axis--y')
-      .call(d3Axis.axisLeft(this.y))
-      .append('text')
-      .attr('class', 'axis-title')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 6)
-      .attr('dy', '0.71em')
-      .attr('text-anchor', 'end')
-      .attr('fill', 'rgb(34, 167, 240)')
-      .attr('font-size', '50')
-      .text('viewers');
-  }
 
-  drawChart() {
-    this.g.selectAll('.bar')
-      .data(this.barData)
+    const xExtent = fc.extentDate()
+    .accessors([d => d.date]);
+  const yExtent = fc.extentLinear()
+    .pad([0.1, 0.1])
+    .accessors([d => d.high, d => d.low]);
+
+  const lineSeries = fc
+    .seriesSvgLine()
+    .mainValue(d => d.high)
+    .crossValue(d => d.date);
+  const areaSeries = fc
+    .seriesSvgArea()
+    .baseValue(d => yExtent(data)[0])
+    .mainValue(d => d.high)
+    .crossValue(d => d.date);
+  const gridlines = fc
+    .annotationSvgGridline()
+    .yTicks(5)
+    .xTicks(0);
+  // average
+  const ma = fc
+    .indicatorMovingAverage()
+    .value(d => d.high)
+    .period(15);
+
+  const maData = ma(data);
+  data = data.map((d, i) => ({ ma: maData[i], ...d }));
+  const movingAverageSeries = fc
+  .seriesSvgLine()
+  .mainValue(d => d.ma)
+  .crossValue(d => d.date)
+  .decorate(sel =>
+    sel.enter()
+      .classed("ma", true)
+  );
+  // volumn
+  const volumeExtent = fc
+    .extentLinear()
+    .include([0])
+    .pad([0, 2])
+    .accessors([d => d.volume]);
+  const volumeDomain = volumeExtent(data);
+  const volumeToPriceScale = d3
+  .scaleLinear()
+  .domain(volumeDomain)
+  .range(yExtent(data));
+  const volumeSeries = fc
+  .seriesSvgBar()
+  .bandwidth(2)
+  .crossValue(d => d.date)
+  .mainValue(d => volumeToPriceScale(d.volume))
+  .decorate(sel =>
+    sel
       .enter()
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('fill', 'rgb(34, 167, 240)')
-      .attr('x', (d) => this.x(d.season))
-      .attr('y', (d) => this.y(d.viewers))
-      .attr('width', this.x.bandwidth())
-      .attr('height', (d) => this.height - this.y(d.viewers));
+      .classed("volume", true)
+      .attr("fill", d => (d.open > d.close ? "red" : "green"))
+  );
+  // multi view
+  const multi = fc.seriesSvgMulti()
+    .series([gridlines, areaSeries, lineSeries, movingAverageSeries, volumeSeries]);
+  // legend
+  const legend = () => {
+    const labelJoin = fc.dataJoin("text", "legend-label");
+    const valueJoin = fc.dataJoin("text", "legend-value");
+  
+    const instance = selection => {
+      selection.each((data, selectionIndex, nodes) => {
+        labelJoin(d3.select(nodes[selectionIndex]), data)
+          .attr("transform", (_, i) => "translate(100, " + (i + 1) * 15 + ")")
+          .text(d => d.name);
+  
+        valueJoin(d3.select(nodes[selectionIndex]), data)
+          .attr("transform", (_, i) => "translate(160, " + (i + 1) * 15 + ")")
+          .text(d => d.value);
+      });
+    };
+  
+    return instance;
+  };
+  const dateFormat = d3.timeFormat("%a %H:%M%p");
+  const priceFormat = d3.format(",.2f");
+  const legendData = datum => [
+    { name: "Open", value: priceFormat(datum.open) },
+    { name: "High", value: priceFormat(datum.high) },
+    { name: "Low", value: priceFormat(datum.low) },
+    { name: "Close", value: priceFormat(datum.close) },
+    { name: "Volume", value: priceFormat(datum.volume) }
+  ];
+  const chartLegend = legend();
+  // chart main
+  const chart = fc
+    .chartCartesian(d3.scaleTime(), d3.scaleLinear())
+    .yOrient("right")
+    .yDomain(yExtent(data))
+    .xDomain(xExtent(data))
+    .svgPlotArea(multi)
+    .decorate(sel => {
+      sel
+        .datum(legendData(data[data.length - 1]))
+        .append("svg")
+        .style("grid-column", 3)
+        .style("grid-row", 3)
+        .classed("legend", true)
+        .call(chartLegend);
+    });
+
+  d3.select("#complex-chart")
+    .datum(data)
+    .call(chart);
   }
 }
