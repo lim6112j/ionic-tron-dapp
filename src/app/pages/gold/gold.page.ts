b6ac1fff94c22d584bb7240b303aed9d655ade98
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2020-07-21 16:36:49 +0900

    crosshair works , data source changed for longer data

--- src/app/pages/gold/gold.page.ts
+++ src/app/pages/gold/gold.page.ts
@@ -3,7 +3,6 @@ import {HttpClient} from '@angular/common/http';
 import { Platform } from '@ionic/angular';
 import * as d3 from 'd3';
 import * as fc from 'd3fc';
-import { ImplicitReceiver } from '@angular/compiler';
 
 // const loadDataEndOfDay = d3.csv(url, d => ({
 //   date: new Date(d.Timestamp * 1000),
@@ -51,16 +50,29 @@ export class GoldPage implements OnInit{
       var objs = d3.csvParse(v);
       console.log(objs);
       const data = objs.map(d => ({
-        date: new Date(d.Timestamp * 1000),
+        date: new Date((d.Timestamp as any) * 1000),
         volume: Number(d.volume),
         high: Number(d.high),
         low: Number(d.low),
         open: Number(d.open),
         close: Number(d.close)
       }));
-      console.log(data);
+      // console.log(data);
 
-      this.createChart(data);
+      // this.createChart(data);
+    });
+    this._http.get('assets/yahoo.json', { responseType: 'json'}).subscribe((v:any) => {
+      const chartData = v.chart.result[0];
+      const quoteData = chartData.indicators.quote[0];
+      const result = chartData.timestamp.map((d, i) => ({
+        date: new Date(d * 1000 - 5 * 1000 * 60 * 60),
+        high: quoteData.high[i],
+        low: quoteData.low[i],
+        open: quoteData.open[i],
+        close: quoteData.close[i],
+        volume: quoteData.volume[i]
+      }));
+      this.createChart(result.slice(0, 500));
     });
   }
 
@@ -68,116 +80,166 @@ export class GoldPage implements OnInit{
 
   }
   createChart(data) {
-
-
     const xExtent = fc.extentDate()
-    .accessors([d => d.date]);
-  const yExtent = fc.extentLinear()
-    .pad([0.1, 0.1])
-    .accessors([d => d.high, d => d.low]);
+      .accessors([d => d.date]);
+    const yExtent = fc.extentLinear()
+      .pad([0.1, 0.1])
+      .accessors([d => d.high, d => d.low]);
 
-  const lineSeries = fc
+    const lineSeries = fc
+      .seriesSvgLine()
+      .mainValue(d => d.high)
+      .crossValue(d => d.date);
+    const areaSeries = fc
+      .seriesSvgArea()
+      .baseValue(d => yExtent(data)[0])
+      .mainValue(d => d.high)
+      .crossValue(d => d.date);
+    const gridlines = fc
+      .annotationSvgGridline()
+      .yTicks(5)
+      .xTicks(0);
+    // average
+    const ma = fc
+      .indicatorMovingAverage()
+      .value(d => d.high)
+      .period(15);
+
+    const maData = ma(data);
+    data = data.map((d, i) => ({ ma: maData[i], ...d }));
+    const movingAverageSeries = fc
     .seriesSvgLine()
-    .mainValue(d => d.high)
-    .crossValue(d => d.date);
-  const areaSeries = fc
-    .seriesSvgArea()
-    .baseValue(d => yExtent(data)[0])
-    .mainValue(d => d.high)
-    .crossValue(d => d.date);
-  const gridlines = fc
-    .annotationSvgGridline()
-    .yTicks(5)
-    .xTicks(0);
-  // average
-  const ma = fc
-    .indicatorMovingAverage()
-    .value(d => d.high)
-    .period(15);
-
-  const maData = ma(data);
-  data = data.map((d, i) => ({ ma: maData[i], ...d }));
-  const movingAverageSeries = fc
-  .seriesSvgLine()
-  .mainValue(d => d.ma)
-  .crossValue(d => d.date)
-  .decorate(sel =>
-    sel.enter()
-      .classed("ma", true)
-  );
-  // volumn
-  const volumeExtent = fc
-    .extentLinear()
-    .include([0])
-    .pad([0, 2])
-    .accessors([d => d.volume]);
-  const volumeDomain = volumeExtent(data);
-  const volumeToPriceScale = d3
-  .scaleLinear()
-  .domain(volumeDomain)
-  .range(yExtent(data));
-  const volumeSeries = fc
-  .seriesSvgBar()
-  .bandwidth(2)
-  .crossValue(d => d.date)
-  .mainValue(d => volumeToPriceScale(d.volume))
-  .decorate(sel =>
-    sel
-      .enter()
-      .classed("volume", true)
-      .attr("fill", d => (d.open > d.close ? "red" : "green"))
-  );
-  // multi view
-  const multi = fc.seriesSvgMulti()
-    .series([gridlines, areaSeries, lineSeries, movingAverageSeries, volumeSeries]);
-  // legend
-  const legend = () => {
-    const labelJoin = fc.dataJoin("text", "legend-label");
-    const valueJoin = fc.dataJoin("text", "legend-value");
-  
-    const instance = selection => {
-      selection.each((data, selectionIndex, nodes) => {
-        labelJoin(d3.select(nodes[selectionIndex]), data)
-          .attr("transform", (_, i) => "translate(130, " + (i + 1) * 15 + ")")
-          .text(d => d.name);
-  
-        valueJoin(d3.select(nodes[selectionIndex]), data)
-          .attr("transform", (_, i) => "translate(190, " + (i + 1) * 15 + ")")
-          .text(d => d.value);
-      });
-    };
-  
-    return instance;
-  };
-  const dateFormat = d3.timeFormat("%a %H:%M%p");
-  const priceFormat = d3.format(",.2f");
-  const legendData = datum => [
-    { name: "Open", value: priceFormat(datum.open) },
-    { name: "High", value: priceFormat(datum.high) },
-    { name: "Low", value: priceFormat(datum.low) },
-    { name: "Close", value: priceFormat(datum.close) },
-    { name: "Volume", value: priceFormat(datum.volume) }
-  ];
-  const chartLegend = legend();
-  // chart main
-  const chart = fc
-    .chartCartesian(d3.scaleTime(), d3.scaleLinear())
-    .yOrient("right")
-    .yDomain(yExtent(data))
-    .xDomain(xExtent(data))
-    .svgPlotArea(multi)
-    .decorate(sel => {
+    .mainValue(d => d.ma)
+    .crossValue(d => d.date)
+    .decorate(sel =>
+      sel.enter()
+        .classed("ma", true)
+    );
+    // volumn
+    const volumeExtent = fc
+      .extentLinear()
+      .include([0])
+      .pad([0, 2])
+      .accessors([d => d.volume]);
+    const volumeDomain = volumeExtent(data);
+    const volumeToPriceScale = d3
+    .scaleLinear()
+    .domain(volumeDomain)
+    .range(yExtent(data));
+    const volumeSeries = fc
+    .seriesSvgBar()
+    .bandwidth(2)
+    .crossValue(d => d.date)
+    .mainValue(d => volumeToPriceScale(d.volume))
+    .decorate(sel =>
       sel
-        .datum(legendData(data[data.length - 1]))
-        .append("svg")
-        .style("grid-column", 3)
-        .style("grid-row", 3)
-        .classed("legend", true)
-        .call(chartLegend);
-    });
+        .enter()
+        .classed("volume", true)
+        .attr("fill", d => (d.open > d.close ? "red" : "green"))
+    );
+        // trading hours - discontinuous time x axis
+        const tradingHours = dates => {
+          const getDateKey = date =>
+            date.getMonth() + "-" + date.getDate() + "-" + date.getFullYear();
+        
+          const tradingHours = dates.reduce((acc, curr) => {
+            const dateKey = getDateKey(curr);
+            if (!acc.hasOwnProperty(dateKey)) {
+              acc[dateKey] = [curr, curr];
+            } else {
+              acc[dateKey][1] = curr;
+            }
+            return acc;
+          }, {});
+        
+          return Object.keys(tradingHours).map(d => tradingHours[d]);
+        };
+        const xScale = fc.scaleDiscontinuous(d3.scaleTime());
+        const tradingHoursArray = tradingHours(data.map(d => d.date));
+        const discontinuities = d3
+          .pairs(tradingHoursArray)
+          .map(d => [d[0][1], d[1][0]]);
+        xScale.discontinuityProvider(fc.discontinuityRange(...discontinuities));
+        const yScale = d3.scaleLinear();
+    // crosshair
+    const crosshair = fc
+    .annotationSvgCrosshair()
+    .x(d => xScale(d.date))
+    .y(d => yScale);
+    // multi view
+    const multi = fc.seriesSvgMulti()
+      .series([gridlines, areaSeries, lineSeries, movingAverageSeries, volumeSeries]);
+    // legend
+    const legend = () => {
+      const labelJoin = fc.dataJoin("text", "legend-label");
+      const valueJoin = fc.dataJoin("text", "legend-value");
+    
+      const instance = selection => {
+        selection.each((data, selectionIndex, nodes) => {
+          labelJoin(d3.select(nodes[selectionIndex]), data)
+            .attr("transform", (_, i) => "translate(130, " + (i + 1) * 15 + ")")
+            .text(d => d.name);
+    
+          valueJoin(d3.select(nodes[selectionIndex]), data)
+            .attr("transform", (_, i) => "translate(190, " + (i + 1) * 15 + ")")
+            .text(d => d.value);
+        });
+      };
+    
+      return instance;
+    };
+    const dateFormat = d3.timeFormat("%a %H:%M%p");
+    const priceFormat = d3.format(",.2f");
+    const legendData = datum => [
+      { name: "Open", value: priceFormat(datum.open) },
+      { name: "High", value: priceFormat(datum.high) },
+      { name: "Low", value: priceFormat(datum.low) },
+      { name: "Close", value: priceFormat(datum.close) },
+      { name: "Volume", value: priceFormat(datum.volume) }
+    ];
+    const chartLegend = legend();
 
-  d3.select("#complex-chart")
-    .datum(data)
-    .call(chart);
-  }
+    // chart main
+    const chart = fc
+      .chartCartesian(xScale, yScale)
+      .yOrient("right")
+      .yDomain(yExtent(data))
+      .xDomain(xExtent(data))
+      .svgPlotArea(multi)
+      .decorate(sel => {
+        sel
+          .datum(legendData(data[data.length - 1]))
+          .append("svg")
+          .style("grid-column", 3)
+          .style("grid-row", 3)
+          .classed("legend", true)
+          .call(chartLegend);
+      });
+
+    // d3.select("#complex-chart")
+    //   .datum(data)
+    //   .call(chart);
+
+    // for crosshair
+    const chartData: any = []
+    const render = () => {
+      d3.select("#complex-chart")
+        .datum(data)
+        .call(chart);
+    
+      const pointer = fc.pointer().on("point", event => {
+        chartData.crosshair = event.map(({x}) => {
+          const closestIndex = d3.bisector((d:any) => d.date)
+            .left(data, xScale.invert(x));
+          return data[closestIndex];
+        });
+        console.log(chartData.crosshair)
+        render();
+      });
+    
+      d3.select("#complex-chart .plot-area").call(pointer);
+    };
+    
+    render();
+    }
 }
